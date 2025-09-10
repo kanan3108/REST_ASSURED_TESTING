ab8602eec18d7c13fd4978fc7dd756337c9b2dd6
Kanan Nawani <nawaniankit69@gmail.com>
2025-09-11 01:56:30 +0530

    success 200

--- src/main/java/org/example/TEST1.java
+++ src/main/java/org/example/TEST1.java
@@ -1,32 +1,23 @@
 package org.example;
 
+import data_payload.*;
 import io.restassured.RestAssured;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class TEST1 {
     public static void main(String[] args) {
         RestAssured.baseURI="https://rahulshettyacademy.com";
-        given().log().all().queryParam("key","qaclick123").
+        String s = given().log().all().queryParam("key","qaclick123").
                 header("Content-Type","application/json").
-                body("{\n" +
-                        "  \"location\": {\n" +
-                        "    \"lat\": -38.383494,\n" +
-                        "    \"lng\": 33.427362\n" +
-                        "  },\n" +
-                        "  \"accuracy\": 50,\n" +
-                        "  \"name\": \"Frontline house\",\n" +
-                        "  \"phone_number\": \"(+91) 983 893 3937\",\n" +
-                        "  \"address\": \"29, side layout, cohen 09\",\n" +
-                        "  \"types\": [\n" +
-                        "    \"shoe park\",\n" +
-                        "    \"shop\"\n" +
-                        "  ],\n" +
-                        "  \"website\": \"http://google.com\",\n" +
-                        "  \"language\": \"French-IN\"\n" +
-                        "}").when().post("maps/api/place/add/json").
-                then().log().all().assertThat().statusCode(200);
+                body(payload.request()).when().post("maps/api/place/add/json").
+                then().log().all().assertThat().statusCode(200).
+                body("scope",equalTo("APP")).
+                header("Access-Control-Max-Age","3600").extract().response().asString();
 
+        //print response
+        System.out.print("----------------------------"+"\n"+s);
 
 
 
